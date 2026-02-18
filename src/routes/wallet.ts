eb1b1bab5e5a202d7bc9e4002a71a2e80f56466a
surajbkale <surajkalecsit@gmail.com>
2026-02-18 23:11:07 +0530

    add POST route for wallet topup

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -2,6 +2,8 @@ import { Router, type Request, type Response } from "express";
 import { db } from "../db/index.js";
 import { users, wallets, assetTypes } from "../db/schema.js";
 import { eq, and } from "drizzle-orm";
+import { topUpWallet } from "../services/wallet.js";
+import { topUpSchema } from "../validators/wallet.js";
 
 const router: Router = Router();
 
@@ -40,5 +42,40 @@ router.get(
     }
   },
 );
+
+router.post("/topup", async (req: Request, res: Response) => {
+  try {
+    const parsed = topUpSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+      return res.status(400).json({
+        message: "Invalid inputs",
+        errors: parsed.error.issues,
+      });
+    }
+
+    const { userId, assetName, amount, referenceId } = parsed.data;
+
+    const result = await topUpWallet(
+      userId,
+      assetName,
+      Number(amount),
+      referenceId,
+    );
+
+    return res.status(200).json(result);
+  } catch (error: any) {
+    console.error("Error while wallet topup", error);
+    if (error.message.includes("Conflict")) {
+      return res.status(409).json({
+        message: "Retry request",
+      });
+    }
+
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 export default router;
